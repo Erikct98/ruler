24d9bdacc54806e72bb628f2f034dac36eddeff8
Jurriën van Winden <jur.development@gmail.com>
2020-11-28 14:27:07 +0100

    added stubs for convex hull

---
+++ unity/Assets/Scripts/RubberBanding/Model/rbConvexHull.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rbConvexHull : MonoBehaviour {
+    
+	
+    void BuildConvexHull(List<rbPoint> points)
+    {
+        
+    }
+
+    bool RemovePoint(rbPoint p)
+    {
+        /*
+         * TODO: remove p from tree
+         * return if success
+         */
+        return false;
+    }
+
+    void UpdateConvexHull(){
+        
+    }
+}
