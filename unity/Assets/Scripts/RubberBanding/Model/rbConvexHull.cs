840f47b6cbb7076253c766c7b7b124bde1cc5d63
Paul Broeckx <paul.hoi@hotmail.com>
2020-12-16 15:37:42 +0100

    convex hull added

--- unity/Assets/Scripts/RubberBanding/Model/rbConvexHull.cs
+++ unity/Assets/Scripts/RubberBanding/Model/rbConvexHull.cs
@@ -3,11 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class rbConvexHull : MonoBehaviour {
+
+    List<rbPoint> convexHull;
+    rbPoint pre;
+    rbPoint post;
+    rbPoint removed;
+
+    int CrossProduct(rbPoint A, rbPoint B, rbPoint C)
+    {
+        return (B.Pos.x - A.Pos.x) * (C.Pos.y - A.Pos.y) - (B.Pos.y - A.Pos.y) * (C.Pos.x - A.Pos.x);
+    }
     
 	
     void BuildConvexHull(List<rbPoint> points)
     {
-        
+        if (points.Count() <= 1)
+        {
+            return points;
+        };
+
+        int n = points.Count();
+        List hull = new List<rbPoint>(new rbPoint[n * 2]);
+
+
+        //sort points from smallest to largest based on x then y
+        points.Sort((a,b) => 
+            a.Pos.x == b.Pos.x ? a.Pos.y.CompareTo(b.Pos.y) : a.Pos.x.CompareTo(b.Pos.x));
+
+        //Create lower hull
+        int k = 0;
+        for (int i = 0; i < n; i++)
+        {
+            while(k >= 2 && CrossProduct(hull[k - 2], hull[k - 1], points[i]) <= 0)
+            {
+                k--;
+            }
+            hull[k++] = points[i];
+        }
+
+        //Add upper hull
+        int l = k + 1;
+        for (int i = n - 2; i >= 0; i--)
+        {
+            while (k >= l && CrossProduct(hull[k - 2], hull[k - 1], points[i]) <= 0)
+            {
+                k--;
+            }
+            hull[k++] = points[i];
+        }
+
+        this.convexHull = hull.Take(k - 1).ToList();
     }
 
     bool RemovePoint(rbPoint p)
@@ -16,7 +61,18 @@ public class rbConvexHull : MonoBehaviour {
          * TODO: remove p from tree
          * return if success
          */
-        return false;
+        int n = this.convexHull.Count();
+        for (int i = 0; i < n; i++)
+        {
+            if (this.convexHull[i].Equals(p))
+            {
+                removed = p;
+                pre = this.convexHull[i - 1];
+                post = this.convexHull[i + 1];
+                break;
+            }
+        }
+        return this.convexHull.Remove(p);
     }
 
     void UpdateConvexHull(){
